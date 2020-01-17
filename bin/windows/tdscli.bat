dbe5b6a24b7057846c8eb5ff3578a010f679659a
lwtnb-wrk <leo.watanabe@totvs.com.br>
2020-01-17 11:07:12 -0300

    script tdscli

---
+++ bin/windows/tdscli.bat
@@ -0,0 +1,21 @@
+:: script chamada TDS CLi
+@echo off
+
+SETLOCAL
+
+SET DEBUG=
+SET DEBUG_LOG=
+IF "%DEBUG%"=="1" SET DEBUG_LOG="--log-file=cli-debug.log"
+
+SET LS_DIR=.
+SET LS_EXE=advpls.exe
+SET LS=%LS_DIR%\%LS_EXE%
+
+SET ARGS=%*
+IF DEFINED ARGS SET ARGS=%ARGS:"=\"%
+
+CALL %LS% %DEBUG_LOG% --tdsCliArguments="%ARGS%"
+
+IF "%DEBUG%"=="1" IF %ERRORLEVEL% NEQ 0 echo TDS CLi errorcode [%ERRORLEVEL%]
+
+ENDLOCAL
